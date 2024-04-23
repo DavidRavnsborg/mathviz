8b95a46bf491f54f93bb08bcfa75c304053ad613
David Ravnsborg <david.ravnsborg@gmail.com>
2024-04-22 22:42:15 -0600

    Adding comment giving context to context.py

--- vignettes/context.py
+++ vignettes/context.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+""" This file can be imported to give vignettes access to the mathviz subfolder. """
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import mathviz
