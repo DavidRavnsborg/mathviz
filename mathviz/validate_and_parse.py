ca22ff32c4fd00d7575aaa29414006f8158a90af
David Ravnsborg <david.ravnsborg@gmail.com>
2024-04-22 22:43:11 -0600

    Updating regex and comment (not to be used by 3rd parties without additional effort)

--- mathviz/validate_and_parse.py
+++ mathviz/validate_and_parse.py
@@ -6,15 +6,16 @@ from sympy import sympify, SympifyError
 def validate_and_parse(input_expr):
     """Validates that input strings are valid mathematical expressions.
 
-    NOTE: This is just a first step. Any hosted application accepting 3rd-party user input requires
-    additional string sanitization to mitigate the potential for injection attacks. This is just a
-    starting point.
-    NOTE 2: sympify uses "eval" which can potentially be very dangerous, see:
-    https://github.com/sympy/sympy/issues/10805
+    NOTE: This is sufficient for personal use, but is only a starting point for hosted applications
+    that accept 3rd-party input. Any such application requires additional string sanitization to
+    mitigate the potential for injection attacks.
+    NOTE 2: sympify uses "eval" which can potentially be especially dangerous if accepting 3rd party
+    input, see: https://github.com/sympy/sympy/issues/10805
     """
 
     # Allow only numbers, basic operators, and alphabetic characters for variables
-    if not re.match(r"^[\d+\-*/^()aAbBcCdDqQrRsStTuUvVwWxXyYzZ. ]+$", input_expr):
+    # if not re.match(r"^[\d+\-*/^()aAbBcCdDqQrRsStTuUvVwWxXyYzZ. ]+$", input_expr):
+    if not re.match(r"^[\d+\-*/^()a-zA-Z. ]+$", input_expr):
         raise ValueError("Invalid characters in input.")
 
     try:
