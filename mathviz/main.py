edeecd957b011bbb13fce1f09ac0361102bbf424
David Ravnsborg <david.ravnsborg@gmail.com>
2024-04-24 14:34:16 -0600

    Hooking up NiceGUI table to submissions to manim: - Submit now take arguments from created variables and functions. - NiceGUI Table manages variable rows and adds structure. - Event handlers handle column updates (added "symbol" and "expression" event handlers). - Added VarType enum for independent and dependent variables.

--- mathviz/main.py
+++ mathviz/main.py
@@ -1,31 +1,147 @@
+from mathviz.var_type import VarType
 from mathviz.animate_string_expression import MathExpressionScene
 from manim.utils.file_ops import open_file as open_media_file
 from nicegui import ui
-from nicegui.events import ValueChangeEventArguments
+from nicegui.events import ValueChangeEventArguments, GenericEventArguments
 from string import ascii_lowercase
 from sympy.parsing.sympy_parser import parse_expr
+from uuid import uuid4
+
+
+# Initial conditions
+
+
+var_chars = ["θ"] + list(ascii_lowercase)
+var_char_defaults = ("x", "y", "z")
+columns = [
+    {"name": "symbol", "label": "Symbol", "field": "symbol"},
+    {"name": "expression", "label": "Expression", "field": "expression"},
+]
+var_rows = []
+
+
+# Event handlers
 
 
 def submit(event: ValueChangeEventArguments):
     name = type(event.sender).__name__
     # ui.notify(f"{name}: {input1.value}")
-    ui.notify("Analyzing math expression.")
+    ui.notify("Analyzing math expression...")
     scene = MathExpressionScene(
-        input_expressions=(
-            {"symbol": "t", "expression": "t=t", "domain": [-10, 10, 1]},
-            {"symbol": "y", "expression": input1.value, "range": [-10, 10, 1]},
-            # {"symbol": "y", "expression": "t**2 - 1"},
-        )
+        input_expressions=var_rows
+        # (
+        #     {"symbol": "t", "expression": "t=t", "domain": [-10, 10, 1]},
+        #     {
+        #         "symbol": ui_var1.value,
+        #         "expression": ui_input1.value,
+        #         "range": [-10, 10, 1],
+        #     },
+        #     # {"symbol": "y", "expression": "t**2 - 1"},
+        # )
     )
-    ui.notify("Rendering MP4 file.")
-
+    ui.notify("Rendering MP4 file...")
     scene.render()
+    ui.notify("Opening MP4 file.")
     open_media_file(scene.renderer.file_writer.movie_file_path)
 
 
+def change_symbol(e: GenericEventArguments) -> None:
+    for row in var_rows:
+        if row["id"] == e.args["id"]:
+            row["symbol"] = e.args["symbol"]
+            break
+
+
+def change_expression(e: GenericEventArguments) -> None:
+    for row in var_rows:
+        if row["id"] == e.args["id"]:
+            row["expression"] = e.args["expression"]
+            break
+
+
+def get_unused_var_char(var_rows):
+    for char in var_char_defaults:
+        for row in var_rows:
+            if char == row["symbol"]:
+                continue
+            return char
+    for char in var_chars:
+        for row in var_rows:
+            if char == row["symbol"]:
+                continue
+            return char
+
+
+# Data type creation
+
+
+def independent_var_row(default_value):
+    # with ui.row():
+    #     ui_var = ui.select(var_chars, value=default_value)  # , on_change=show)
+    return {
+        "id": str(uuid4()),
+        "type": VarType.Independent,
+        "symbol": default_value,
+        "expression": "",  # f"{default_value}={default_value}",
+        "domain": [-10, 10, 1],
+    }
+
+
+def function_row(default_value):
+    # with ui.row():
+    #     ui_var = ui.select(var_chars, value=default_value)  # , on_change=show)
+    #     ui_input = ui.input("Function")
+    return {
+        "id": str(uuid4()),
+        "type": VarType.Dependent,
+        "symbol": default_value,
+        "expression": "x^2 + 1",
+        "range": [-10, 10, 1],
+    }
+
+
+# DOM
+
+
 ui.label("Hello Mathviz!")
-with ui.row():
-    var1 = ui.select(list(ascii_lowercase), value="y")  # , on_change=show)
-    input1 = ui.input("Function 1")
-ui.button("Button", on_click=submit)
+
+var_rows.append(independent_var_row("x"))
+var_rows.append(function_row("y"))
+table = ui.table(columns=columns, rows=var_rows).classes("w-1/2")
+table.add_slot(
+    "body-cell-symbol",
+    r'''
+    <q-td key="symbol" :props="props">
+        <q-select
+            v-model="props.row.symbol"
+            :options="'''
+    + str(var_chars)
+    + r""""
+            @update:model-value="() => $parent.$emit('change_symbol', props.row)"
+        />
+    </q-td>
+""",
+)
+table.on("change_symbol", change_symbol)
+table.add_slot(
+    "body-cell-expression",
+    r"""
+    <q-td key="expression" :props="props">
+        <q-input
+            v-model="props.row.expression"
+            @update:model-value="() => $parent.$emit('change_expression', props.row)"
+        />
+    </q-td>
+""",
+)
+table.on("change_expression", change_expression)
+
+ui.button(
+    "+",
+    on_click=lambda: var_rows.append(
+        function_row(default_value=get_unused_var_char(var_rows))
+    ),
+)
+ui.button("debug", on_click=lambda: (print(var_rows), ui.notify(var_rows)))
+ui.button("Render animation", on_click=submit)
 ui.run()
---
+++ mathviz/var_type.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class VarType(Enum):
+    Independent = 1
+    Dependent = 2
