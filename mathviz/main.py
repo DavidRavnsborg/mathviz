d5852b335afb87dc73eb16d6b5ec98a36aea0938
David Ravnsborg <david.ravnsborg@gmail.com>
2024-05-01 00:29:25 -0600

    Adding video display on right pane (stock video for now)

--- mathviz/main.py
+++ mathviz/main.py
@@ -150,5 +150,11 @@ async def index():
                 ),
             )
             ui.button("Render animation", on_click=submit)
+        with splitter.after:
+            video = ui.video(
+                "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4"
+            )
+            video.on("ended", lambda _: ui.notify("Video playback completed"))
+
 
 ui.run(storage_secret="foobar")
